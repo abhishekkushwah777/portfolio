ac000df2c0ee973957261a9e3cbe28f303690a92
abhishekkushwahlocal777 <abhishekkushwah1359@gmail.com>
2025-09-08 07:15:32 +0530

    education page updated

--- src/Education.jsx
+++ src/Education.jsx
@@ -4,12 +4,28 @@ import './Education.css';
 
 const Education = () => {
   return (
-    <>
-      <div className="container">one</div>
-      <div className="container">two</div>
-      <div className="container">three</div>
-      <div className="container">four</div>
-    </>
+    <div className="education-page">
+      <Link to="/" className="back-btn">Back</Link>
+      <div className="edu-nav">
+        <h2>EDUCATION</h2>
+      </div>
+      <div className="edu-main">
+        <div className="edu-left-container">
+          <h3>2025</h3>
+          <h3>2024</h3>
+          <h3>2023</h3>
+          <h3>2022</h3>
+        </div>
+        <div className="edu-right-container">
+          <div className="testdiv"></div>
+          <div className="testdiv"></div>
+          <div className="testdiv"></div>
+          <div className="testdiv"></div>
+          <div className="testdiv"></div>
+        </div>
+      </div>
+      
+    </div>
   );
 };
 
